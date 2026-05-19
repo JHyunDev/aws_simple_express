49749f69adc3da2208ba8498b55d44c877edf064
JHyunDev <galaga1010@naver.com>
2026-05-19 19:01:40 +0900

    delete

--- server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const app = require('./src/app');
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
-});
