360b996a570d4331179d539a0e7039c907aa461b
JHyunDev <galaga1010@naver.com>
2026-05-27 15:54:05 +0900

    update api items

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -10,10 +10,10 @@ router.get('/hello', (req, res) => {
   });
 });
 
-router.get('/items', async (req, res) => {
-  try{
-    const [rows] = await db.query(`
-       SELECT,
+router.get('/items', async (req, res) => { //비동기처리, (클라이언트가 보낸 요청값, 서버가 돌려줄 결과 파일)
+  try{ //const result로 쓰면 mysql2에서 rows와 fields정보가 같이 오므로 const [rows]로 rows정보만 뽑아 사용한다
+    const [rows] = await db.query(` 
+       SELECT
         items.id,
         items.name,
         items.description,
