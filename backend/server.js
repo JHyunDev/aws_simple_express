e1339090da198aeab92e45123eb637b232c58c31
JHyunDev <galaga1010@naver.com>
2026-09-22 18:55:03 +0900

    add git multi deploy systemm

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,6 @@ const app = require('./src/app'); //이 부분을 만날때 Node.js가 app.js실
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000, "0.0.0.0", () => {  // 앞으로 3000번 TCP포트로 들어오는 연결을 이 Express app객체로 처리하겠다, 바인딩 -> 소켓을 특정 IP/포트에 연결
-  console.log("Server running on port 3000");
+app.listen(PORT, "0.0.0.0", () => {  // 앞으로 3000번 TCP포트로 들어오는 연결을 이 Express app객체로 처리하겠다, 바인딩 -> 소켓을 특정 IP/포트에 연결
+  console.log(`Server running on port ${PORT}`);
 });
